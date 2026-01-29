ec93e8114ab17a4f18a607c6830567c4f97add7e
Tyler Kim <tylerjsook@gmail.com>
2026-01-29 15:53:11 -0800

    state management

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,7 +1,14 @@
 "use client";
 
-import useState from "react";
+import { useState } from "react";
 
 export default function Signup() {
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+    email: "",
+    number: "",
+    role: "",
+  });
   return <div></div>;
 }
