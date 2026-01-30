c3d10dadcbe00563c2aac8cfcf8f149dadb023f4
Farid Rohana <faridrohana0@gmail.com>
2026-01-29 22:08:01 -0800

    Add 'use client' directive to Contact component to fix build error

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+"use client";
 
-const Contact = () => {
+import React, { useState } from "react";
+import styles from "../styles/contact.module.css";
+
+export default function Contact() {
+  //variable to store form data inputted by the user
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
   return (
     <div>
       <h2>Contact Us</h2>
-      <p>contact form</p>
+      <form>
+        <div>
+          <label>Name</label>
+          <input type="text" />
+        </div>
+
+        <div>
+          <label>Email</label>
+          <input type="email" />
+        </div>
+
+        <div>
+          <label>Message</label>
+          <textarea></textarea>
+        </div>
+
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
-};
-
-export default Contact;
+}
