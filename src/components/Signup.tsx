d48072b9c496b3db34c501c4cbf8e0d007d9f8e0
Tyler Kim <tylerjsook@gmail.com>
2026-01-29 16:27:46 -0800

    attatched helper function to each input to handle change, added submit button with helper function to handle submit event

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -3,6 +3,15 @@
 import { useState } from "react";
 
 export default function Signup() {
+  const handleChange = (e: { target: { name: any; value: any } }) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log("Form submitted:", formData);
+  };
   const [formData, setFormData] = useState({
     name: "",
     password: "",
@@ -15,27 +24,54 @@ export default function Signup() {
       <h1>Sign Up or Login to your Account</h1>
 
       <div className="form-box">
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <h2>Sign up </h2>
           <label>First & Last Name</label>
           <br />
-          <input type="text" id="name" placeholder="First & Last Name" />
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="First & Last Name"
+          />
           <br />
           <label>Password</label> <br />
-          <input type="password" id="password" placeholder="Password" />
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Password"
+          />
           <br />
           <label>Email</label> <br />
-          <input type="email" id="email" placeholder="Email" />
+          <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" />
           <br />
           <label>Phone Number</label> <br />
-          <input type="tel" id="number" placeholder="Phone Number" />
+          <input type="tel" name="number" value={formData.number} onChange={handleChange} placeholder="Phone Number" />
           <br />
           <label>Are you over 18?</label> <br />
-          <input type="radio" id="age" name="drone" value="Yes" />
-          <label htmlFor="Yes">Yes</label>
-          <input type="radio" id="age" name="drone" value="No" />
-          <label htmlFor="Yes">No</label>
+          <input
+            type="radio"
+            id="over18"
+            name="age"
+            value="Yes"
+            checked={formData.age === "Yes"}
+            onChange={handleChange}
+          />
+          <label htmlFor="over18">Yes</label>
+          <input
+            type="radio"
+            id="under18"
+            name="age"
+            value="No"
+            checked={formData.age === "No"}
+            onChange={handleChange}
+          />
+          <label htmlFor="under18">No</label>
           <br />
+          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
