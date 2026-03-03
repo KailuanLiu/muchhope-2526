f8fab8ae48ed8577c2d132646d0a20a80db1cf4c
Tyler Kim <tylerjsook@gmail.com>
2026-03-02 18:40:43 -0800

    uncommented page tsx

--- frontend/src/Auth/SignUp/page.tsx
+++ frontend/src/Auth/SignUp/page.tsx
@@ -1,9 +1,9 @@
-// import { useSignUp } from "@clerk/nextjs";
-// import Signup from "../../components/Signup";
+import { useSignUp } from "@clerk/nextjs";
+import Signup from "../../components/Signup";
 
-// export default function SignUpPage() {
-//   const { isLoaded, signUp, setActive } = useSignUp();
+export default function SignUpPage() {
+  const { isLoaded, signUp, setActive } = useSignUp();
 
-//   // Pass the Clerk functions as props to your Signup component
-//   // return <Signup signUp={signUp} setActive={setActive} isLoaded={isLoaded} />;
-// }
+  // Pass the Clerk functions as props to your Signup component
+  return <Signup signUp={signUp} setActive={setActive} isLoaded={isLoaded} />;
+}
