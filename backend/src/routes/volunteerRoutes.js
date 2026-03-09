c2b530901f401bac57671cd4e7b9980a10e32dd0
Siddharth Balaji <siddharthbalaji6@gmail.com>
2026-03-08 21:31:50 -0700

    feat: added /login endpoint

--- backend/src/routes/volunteerRoutes.js
+++ backend/src/routes/volunteerRoutes.js
@@ -10,3 +10,17 @@ router.get("/", async (req, res) => {
         res.status(500).json({message: error.message});
     }
 })
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email } = req.body;
+        const volunteer = await Volunteer.findOne({email});
+
+        if (!volunteer) {
+            return res.status(404).json({message: "Volunteer not found"});
+        }
+        res.status(200).json(volunteer);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
