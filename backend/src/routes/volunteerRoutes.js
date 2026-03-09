d1ddedfaa8b3d964a55f85c3c90e92ffc8822e1e
Siddharth Balaji <siddharthbalaji6@gmail.com>
2026-03-08 21:36:41 -0700

    feat: added a put route to edit volunteer information

--- backend/src/routes/volunteerRoutes.js
+++ backend/src/routes/volunteerRoutes.js
@@ -24,3 +24,20 @@ router.post("/login", async (req, res) => {
         res.status(500).json({message: error.message});
     }
 });
+
+router.put("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updates = req.body;
+        const volunteer = await Volunteer.findOneAndUpdate({ id }, updates, {
+            new: true,
+            runValidators: true,
+        });
+        if (!volunteer) {
+            return res.status(404).json({ message: "Volunteer not found"});
+        }
+        res.status(200).json(volunteer);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
