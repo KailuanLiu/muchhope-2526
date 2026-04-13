83e9be79153d2a7b7fd864d76ca0bb0f560caaff
Siddharth Balaji <siddharthbalaji6@gmail.com>
2026-04-12 21:25:36 -0700

    feat: added GET /volunteers/:id/events route

--- apps/backend/src/routes/volunteerRoutes.js
+++ apps/backend/src/routes/volunteerRoutes.js
@@ -26,6 +26,23 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.get("/:id/events", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const volunteer = await Volunteer.findOne({ id });
+    if (!volunteer) {
+      return res.status(404).json({ message: "Volunteer not found" });
+    }
+
+    const events = await Event.find({
+      "volunteers.email": volunteer.email,
+    });
+    res.status(200).json(events);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 router.delete("/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -49,7 +66,7 @@ router.put("/:id/make-event-admin", async (req, res) => {
     if (!volunteer) {
       return res.status(404).json({ message: "Volunteer not found" });
     }
-    
+
     // check if the event exists and error if it doesn't
     const event = await Event.findById(eventId);
     if (!event) {
