699ce16ca5465e79df7ea701a50fa2344f41a42c
Kailuan Liu <kailuan1127@gmail.com>
2026-04-20 11:51:33 -0700

    upd: merged about page

--- frontend/src/app/About/page.tsx
+++ frontend/src/app/About/page.tsx
@@ -2,7 +2,7 @@ import About from "../../components/About";
 
 export default function AboutUsPage() {
   return (
-    <main style={{ display: "flex", height: "100vh", overflow: "hidden" }}>
+    <main style={{ display: "flex", height: "100vh", overflowY: "auto" }}>
       <About />
     </main>
   );
--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -2,8 +2,14 @@
 
 import styles from "../styles/about.module.css";
 import AuthLayout from "../app/AuthLayout";
+import { useRouter } from "next/navigation";
 
 export default function About() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/ContactUs");
+  };
   return (
     <AuthLayout>
       <div className={styles.aboutContainer}>
@@ -15,6 +21,22 @@ export default function About() {
           purchasing needed materials with self-contributed funds. Emotional and spiritual needs are addressed by
           conversation and - whenever possible - prayer with each homeless person.
         </p>
+      </div>
+      <div className={styles.aboutContactContainer}>
+        <img src="/placeholder.jpg" alt="placeholder" className={styles.aboutContactImage} />
+
+        <div className={styles.aboutContactInfo}>
+          <h2 className={styles.aboutContactTitle}> Sujana </h2>
+          <p className={styles.aboutContactDesc}>
+            {" "}
+            Information about Sujana. The dedicated staff of Much Hope endeavors to provide for the physical needs of
+            the homeless community by purchasing needed materials with self-contributed funds. Emotional and spiritual
+            needs are addressed by conversation and - whenever possible - prayer with each homeless person.{" "}
+          </p>
+          <button type="button" onClick={handleClick} className={styles.aboutContactButton}>
+            Contact
+          </button>
+        </div>
       </div>
     </AuthLayout>
   );
