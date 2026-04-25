74a978593a095be9647048e4acfbfb28a6e77999
Kailuan Liu <kailuan1127@gmail.com>
2026-04-25 16:36:01 -0700

    change: adminnavbar

--- apps/frontend/src/components/admin/AdminNavbar.tsx
+++ apps/frontend/src/components/admin/AdminNavbar.tsx
@@ -26,6 +26,8 @@ export default function AdminNavbar({
     { label: "My Profile", href: "/admin/profile", icon: "/icons/user-alt-1.svg" },
     { label: "Manage Events", href: "/Admin/Events", icon: "/icons/calendar-lines-pen.svg" },
     { label: "View Volunteers", href: "/Admin/Volunteers", icon: "/icons/users.svg" },
+
+    // TODO: make about us and contact us editable and make sure its connected to the backend
     { label: "About Us", href: "/About", icon: "/icons/circle-information.svg" },
     { label: "Contact Us", href: "/ContactUs", icon: "/icons/send-icon.svg" },
   ];
